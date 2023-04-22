a26452e5bd4eec6a3a2c098c00f35dc166aca66b
Sanchit33 <shindesanchit33@gmail.com>
2023-04-23 00:18:06 +0530

    23-04-2023

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ import express from 'express'
 // const express = require('express')
 import colors from 'colors'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
 
 // configure env
 dotenv.config();
 
-//rest object
+// database config
+connectDB();
 
+//rest object
 const app = express()
+
+
+// Middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+// routes
+app.use('/api/v1/auth', authRoutes);
 
 // rest api
 app.get('/', (req,res)=>{
